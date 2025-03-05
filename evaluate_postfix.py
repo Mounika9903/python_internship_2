e723db67d0a3833e743c52a4136e34add36209d8
Mounika9903 <167081754+Mounika9903@users.noreply.github.com>
2025-03-05 21:30:11 +0530

    Add files via upload

---
+++ evaluate_postfix.py
@@ -0,0 +1,19 @@
+def evaluate_postfix(expression):
+    stack = []
+    for char in expression:
+        if char.isdigit():
+            stack.append(int(char))
+        else:
+            b = stack.pop()
+            a = stack.pop()
+            if char == '+':
+                stack.append(a + b)
+            elif char == '-':
+                stack.append(a - b)
+            elif char == '*':
+                stack.append(a * b)
+            elif char == '/':
+                stack.append(a / b)
+    return stack[0]
+expr = "23*2+"
+print(evaluate_postfix(expr)) 
